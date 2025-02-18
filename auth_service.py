d1ef75148e549a1f6aba10d467958c7e267dfade
Poshtaklon <152329594+YuliaDruzhynina@users.noreply.github.com>
2025-02-18 16:08:24 +0200

    Update auth_service.py

--- auth_service.py
+++ auth_service.py
@@ -1,1 +1,13 @@
-# logic auth (jwt, hashing passw)
+
+from app.database.models import Image, Role, User
+from app.database.connection import get_db
+from app.schemas import ImageCreate, ImageResponseSchema
+from app.services.auth_service import get_current_user   #зробити корректн import!
+from app.settings import settings
+
+router = APIRouter(tags=['images'])
+
+cloudinary.config(
+    cloud_name=settings.CLD_NAME, 
+    api_key=settings.CLD_API_KEY,   
+    api_secret=settings.CLD_API_SECRET  #add to .env!# logic auth (jwt, hashing passw)
