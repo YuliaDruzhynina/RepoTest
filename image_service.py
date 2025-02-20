36cf24d9db9c1ba309e24920c8ef53ec34b2ceac
Poshtaklon <152329594+YuliaDruzhynina@users.noreply.github.com>
2025-02-20 08:03:59 +0200

    Add files via upload

--- image_service.py
+++ image_service.py
@@ -1,58 +1,54 @@
-# itegration with Cloudinary, image processing, upload images to the cloud
-from typing import Optional, List
-
-from fastapi import status, APIRouter, Depends, HTTPException, Query, UploadFile, File
-from sqlalchemy.ext.asyncio import AsyncSession
-
+# itegration with Cloudinary, image processing, upload images to the cloudinary
 import cloudinary
 import cloudinary.uploader
-import cloudinary.api
 
-from app.database.models import Image, Role, User
-from app.database.connection import get_db
-from app.schemas import ImageCreate, ImageResponseSchema
-from app.services.auth_service import get_current_user   #зробити корректн import!
-from app.settings import settings
+from typing import List, Optional
+from fastapi import HTTPException, File, UploadFile, Query, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
-router = APIRouter(tags=['images'])
+from app.database.models import Image, User
+from app.services.security.auth_service import auth_serv
+from app.database.connection import  get_conn_db
+from app.config import settings
+
 
 cloudinary.config(
     cloud_name=settings.CLD_NAME, 
     api_key=settings.CLD_API_KEY,   
-    api_secret=settings.CLD_API_SECRET  #add to .env!
+    api_secret=settings.CLD_API_SECRET  
 )
 
-@router.post("/upload_image/",
-             dependencies=[Depends(RoleChecker([Role.user, Role.admin, Role.moderator]))], #correct name-import!
-             response_model=ImageCreate,
-             status_code=status.HTTP_201_CREATED)
-
-async def upload_file(description: str,
+async def upload_image(description: str,
                       file: UploadFile = File(...),
                       tags: Optional[List[str]] = Query([]),
-                      db: AsyncSession = Depends(get_db),
-                      current_user: User = Depends(get_current_user)) -> dict:
-    
+                      db: AsyncSession = Depends(get_conn_db),
+                      current_user: User = Depends(auth_serv.get_current_user)
+                      ) -> dict:
+
     if len(tags) > 5:
         raise HTTPException(status_code=400, detail="You can only add up to 5 tags.")
-
+    
+    # download image cloud cloudinary
     try:
-        # Upload file to Cloudinary
         result = cloudinary.uploader.upload(file.file, folder=current_user.email)
         secure_url = result["secure_url"]
-
-     # Create a new image
-        db_file = Image(url=secure_url,
-                       description=description,
-                       owner_id=current_user.id)
-        db.add(db_file)
-        await db.commit()
-        await db.refresh(db_file)
-
-        return {
-                "url": secure_url,
-                "description": db_file.description,
-                "owner_id": db_file.owner_id
-                }
+        #public_id = result["public_id"]
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail="Error uploading file to Cloudinary: " + str(e))
+
+    # create new post/image in DB
+    db_file = Image(image_url=secure_url,
+                    description=description,
+                    user_id="current_user.id",
+                    #public_id=public_id
+                    )
+    db.add(db_file)
+    await db.commit()
+    await db.refresh(db_file)
+
+    return {
+        "url": secure_url,
+        "description": db_file.description,
+        "user_id": db_file.user_id
+    }
+
---
+++ images.py
@@ -0,0 +1,39 @@
+# routes by upload images & transformations
+from fastapi import APIRouter, File, UploadFile, status, Depends, Query
+from typing import List, Optional
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.services.image_service import upload_image as ui
+from app.database.connection import get_conn_db
+from app.services.security.auth_service import auth_serv
+from app.database.models import User
+from app.schemas import ImageResponseSchema
+from app.repository.images import delete_image, update_image_description, get_image_by_url
+
+router = APIRouter(tags=['images'])
+
+@router.post("/upload_image/",
+             response_model=ImageResponseSchema,
+             status_code=status.HTTP_201_CREATED)
+async def upload_image(description: str,
+                      file: UploadFile = File(...),
+                      tags: Optional[List[str]] = Query([]),
+                      db: AsyncSession = Depends(get_conn_db),
+                      current_user: User = Depends(auth_serv.get_current_user)
+                      ) -> dict:
+    return await ui(description, file, tags, db, current_user)
+
+@router.delete("/delete_image/{image_id}/", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_image(image_id: int, db: AsyncSession = Depends(get_conn_db), current_user: User = Depends(auth_serv.get_current_user)):
+    await delete_image(image_id, db, current_user)
+
+@router.put("/update_image_description/{image_id}/", response_model=ImageResponseSchema)
+async def update_image_description(image_id: int, description: str, db: AsyncSession = Depends(get_conn_db), current_user: User = Depends(auth_serv.get_current_user)):
+    return await update_image_description(image_id, description, db, current_user)
+
+@router.get("/get_image/{image_url}/", response_model=ImageResponseSchema)
+async def get_image_by_url(image_url: str, db: AsyncSession = Depends(get_conn_db)):
+    return await get_image_by_url(image_url, db)
+
+
+
